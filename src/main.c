13fa793af7a76874ad600b9bcb3e05eb2fbbc0d4
scotty <scottyndanga02400@gmail.com>
2025-10-30 15:12:34 +0100

    add blink_y=task

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void blink_task(void *pvParameter) 
+{
+    while (1)
+    {
+        printf("Led ON\n");
+        vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 milliseconds
+        printf("Led OFF\n");
+        vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 milliseconds
+    }
+}
+
+
+
+
+
+void app_main()
+{
+    printf("=== Début du programme ===\n");
+
+    xTaskCreate(&blink_task, "blink_task", 2048, NULL, 1, NULL);
+}
