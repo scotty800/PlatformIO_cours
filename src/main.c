8ad04a7a12b79a865196504450fd59db1e00c992
scotty <scottyndanga02400@gmail.com>
2025-10-31 14:52:37 +0100

    mutex exercice

--- src/main.c
+++ src/main.c
@@ -1,25 +1,56 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 
-void blink_task(void *pvParameter) 
+SemaphoreHandle_t uart_mutex;
+
+void task_A(void *pvParameter)
 {
-    while (1)
+    while(1)
     {
-        printf("Led ON\n");
-        vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 milliseconds
-        printf("Led OFF\n");
-        vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 milliseconds
+        if (xSemaphoreTake(uart_mutex, portMAX_DELAY) == pdTRUE)
+        {
+            printf("[Task A] Début envoie uart\n");
+            vTaskDelay(pdMS_TO_TICKS(200));
+            printf("[Task A] Fin envoie uart\n");
+            xSemaphoreGive(uart_mutex);
+        }
     }
+    vTaskDelay(pdMS_TO_TICKS(500));
 }
 
-
-
-
+void task_B(void *pvParameter)
+{
+    while(1)
+    {
+        if (xSemaphoreTake(uart_mutex, portMAX_DELAY) == pdTRUE)
+        {
+            printf("[Task B] Début envoie uart\n");
+            vTaskDelay(pdMS_TO_TICKS(200));
+            printf("[Task B] Fin envoie uart\n");
+            xSemaphoreGive(uart_mutex);
+        }
+    }
+    vTaskDelay(pdMS_TO_TICKS(500));
+}
 
 void app_main()
 {
-    printf("=== Début du programme ===\n");
+    printf("\n=== Simulation Mutex UART ===\n");
 
-    xTaskCreate(&blink_task, "blink_task", 2048, NULL, 1, NULL);
+    uart_mutex = xSemaphoreCreateBinary();
+    if (uart_mutex == NULL)
+    {
+        printf("Erreur création mutex\n");
+        while(1);
+    }
+    else
+    {
+        printf("Mutex créé avec succès\n");
+    }
+
+
+    xTaskCreate(task_A, "Task A", 2048, NULL, 2, NULL);
+    xTaskCreate(task_B, "Task B", 2048, NULL, 2, NULL);
 }
